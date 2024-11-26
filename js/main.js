e650b641da4dceec7ce2de05ab93213a76093181
Mary Pope <marypope19@gmail.com>
2024-11-26 20:13:51 +0100

    filmsida

---
+++ js/main.js
@@ -0,0 +1,32 @@
+//Funktioner för varje film som gör att en alert dyker med en beskrivning av filmen
+function moreInfoInterstellar() {
+    alert("En grupp forskare får ett avgörande uppdrag - att resa till andra stjärnsystem och utforska om vi har en framtid i rymden.");
+}
+
+function moreInfoDune() {
+    alert("Paul Atreides, en briljant och talangfull ung man född med ett storslaget öde, måste resa till den farligaste planeten i universum för att säkra sin familjs och sitt folks framtid. När illasinnade krafter kastas in i en konflikt över tillgången på planetens mest värdefulla råvara – ett ämne med förmåga att låsa upp mänsklighetens fulla potential, kommer bara de som övervinner sina rädslor att överleva.");
+}
+
+function moreInfoHammarskjold() {
+    alert("1961 har Dag Hammarskjöld ett år kvar på sin post som FN:s Generalsekreterare. Därefter ska han flytta till sitt hus på Österlen och den efterlängtade friheten. Men samtidigt rasar det kalla kriget för fullt, och brännpunkten ligger i Kongo. Dag Hammarskjöld tar på sig uppdraget att mäkla fred och enighet i landet. Ett omöjligt uppdrag, enligt många. Ett självmordsuppdrag, enligt andra.");
+}
+
+function moreInfoDenSista() {
+    alert("När Lars Hammar efter 40 år som älskad fransklärare i Köping går i pension ser han framför sig en ljuvlig “tredje ålder” med resor, vin och upplevelser tillsammans med sin fru Tiina. Men det blir inte som han tänkt sig. I stället för att ta för sig av livets goda blir Lars alltmer passiv och viljelös hemma i sin läderfåtölj, till både Tiinas och deras son Filips stora frustration, och förtvivlan. Då får Filip en idé, att ta sin skröpliga gamla far till hans älskade Frankrike och låta honom återse de platser som han älskat mest i livet. Genom att göra samma bilresa som familjen brukade göra när Filip var barn och i hemlighet iscensätta några av livets härligaste ögonblick hoppas han kunna tända Lars livsgnista igen. Till sin hjälp tar Filip sin bästa vän Fredrik, några rejäla doser optimism och påhittighet samt en gammal orange Renault 4. Äventyret kan börja!");
+}
+
+function moreInfoOppenheimer() {
+    alert("Berättelsen om den amerikanske vetenskapsmannen J. Robert Oppenheimer och hans roll i utvecklingen av atombomben.");
+}
+
+function moreInfoDarkKnight() {
+    alert("Nu har den mörke riddaren bestämt sig: han ska en gång för alla få ett slut på den organiserade brottsligheten i Gotham City. Till sin hjälp har han polischef Jim Gordon och åklagare Harvey Dent - och de visar sig vara en sällsynt bra brottsbekämpartrio. Men snart stöter de på patrull, då den genialiske mästerskurken Jokern träder in på spelplanen. Han skapar snabbt total anarki i staden och tvingar Batman farligt nära fel sida av lagen.");
+}
+
+function moreInfoOve() {
+    alert("59-årige Ove är kvarterets vresige ordningsman som flera år tidigare avsattes som ordförande i bostadsrättsföreningen, men det ger han blanka fan i och ser med järnhand över kvarteret. När höggravida Parvaneh med familj flyttar in i radhuset mittemot och backar in i Oves brevlåda blir det upptakten till en komisk och hjärtevärmande historia om tilltufsade katter, oväntad vänskap och kärlek så stark att bara döden ser ut som den enda utvägen. Ove är en man som vet att rätt ska vara rätt och man ska hjälpa sin medmänniska och som med stor träffsäkerhet betraktar sin samtid.");
+}
+
+function moreInfoMammaMu() {
+    alert("När Mamma Mu blir inspirerad av barnen och vill göra sin alldeles egna Mu-sikal, råkar hon ställa till det rejält. Lillebrors älskade nalle försvinner under mystiska omständigheter och det är Mamma Mus fel. 'Kan du inte bara vara en vanlig ko?' undrar hennes vän Kråkan. 'Då händer ingenting!' Men det är ju precis det Mamma Mu vill undvika. Hon älskar när det händer saker! Inte bara så sorgliga saker. Tillsammans börjar Mamma Mu och Kråkan följa spåren efter den saknade nallen.");
+}
